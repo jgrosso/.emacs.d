01f1b0c5bf47151502fef4bf4cb596fbc29658ae
Joshua Grosso <jgrosso1@asu.edu>
2017-01-08 16:54:36 -0700

    Add ERB support

--- customizations/setup-web.el
+++ customizations/setup-web.el
@@ -1,8 +1,11 @@
 (require 'web-mode)
+
 (add-to-list 'auto-mode-alist '("\\.jsx?$" . web-mode))
 (add-hook 'web-mode-hook
           (lambda ()
             (web-mode-set-content-type "jsx")))
+
+(add-to-list 'auto-mode-alist '("\\.html.erb?$" . web-mode))
 
 (setq web-mode-markup-indent-offset 2)
 (setq web-mode-css-indent-offset 2)
