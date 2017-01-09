3b2d7f275f801fb10fa6512e49c8d70d98769360
Joshua Grosso <jgrosso1@asu.edu>
2017-01-08 17:43:31 -0700

    Remove unnecessary `delete-line` (replaced by `:dd` in `evil-mode`)

--- customizations/editing.el
+++ customizations/editing.el
@@ -1,11 +1,3 @@
 (global-subword-mode)
 
 (global-company-mode)
-
-(fset 'delete-line
-      (lambda (&optional arg)
-        "Delete the line the cursor is currently located on."
-        (interactive "p")
-        (kmacro-exec-ring-item
-         (quote ([1 S-down backspace] 0 "%d")) arg)))
-(global-set-key (kbd "C-c l k") 'delete-line)
