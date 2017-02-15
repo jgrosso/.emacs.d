ecc0c30f04b6674d7548675b00a2951f70a3691f
Joshua Grosso <joshuag@axosoft.com>
2017-02-14 20:34:35 -0700

    Don't default to JSX support for web files (`web-mode` support is buggy)

--- customizations/setup-web.el
+++ customizations/setup-web.el
@@ -1,9 +1,6 @@
 (require 'web-mode)
 
 (add-to-list 'auto-mode-alist '("\\.jsx?$" . web-mode))
-(add-hook 'web-mode-hook
-          (lambda ()
-            (web-mode-set-content-type "jsx")))
 
 (add-to-list 'auto-mode-alist '("\\.html.erb?$" . web-mode))
 
