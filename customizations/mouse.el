f923601b5fd348782a51f256eee25e5eb473b772
Joshua Grosso <joshuag@axosoft.com>
2017-02-14 20:23:47 -0700

    Fix `set-scroll-speed`

--- customizations/mouse.el
+++ customizations/mouse.el
@@ -2,7 +2,7 @@
 
 (defun set-scroll-speed (speed)
   "Set scroll speed"
-  (setq mouse-wheel-scroll-amount '(speed ((shift . speed) ((control) . nil)))))
+  (setq mouse-wheel-scroll-amount `(,speed ((shift . ,speed) ((control) . nil)))))
 
 (defun set-trackpad-scroll ()
   "Apply trackpad scroll speed defaults"
@@ -12,5 +12,5 @@
 (defun set-mouse-scroll ()
   "Apply mouse scroll speed defaults"
   (interactive)
-  (set-scroll-speed 10))
+  (set-scroll-speed 5))
 (set-mouse-scroll)
